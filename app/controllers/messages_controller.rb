6d66f84e0c9aac6d89dfce29a795c195fe71f88d
masoud arabi <arabimasoud88@gmail.com>
2021-02-08 15:44:00 -0500

    add action cable to project

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,17 @@
 class MessagesController < ApplicationController
 
     def create
-        raise
-        @message = message.new(message_params)
+        @message = Message.new(message_params)
         @chatroom = Chatroom.find(params[:chatroom_id])
         @message.chatroom = @chatroom
         @message.user = current_user
 
         if @message.save
-            redirect_to chatroom_path(@chatroom)
+            ChatroomChannel.broadcast_to(
+                @chatroom,
+                render_to_string(partial: "message", locals: {message: @message})
+            )
+            redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
         else
             render 'chatrooms/show'
         end
